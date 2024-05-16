da62a2be61383a4c72b5aa05c3200ad68eccd563
fyfly9090 <fengyun0706@gmail.com>
2024-05-16 13:53:52 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Labs from "./Labs";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to Web Dev</h1>
+      <Labs/>
+    </div>
+  );
+}
+
+export default App;
