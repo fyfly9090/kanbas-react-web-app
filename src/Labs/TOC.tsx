87ea9d5f3f04bc0ac86ea6050f74b73b5c53be2b
fyfly9090 <fengyun0706@gmail.com>
2024-05-19 21:55:42 -0400

    added github URL

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,6 +7,11 @@ export default function TOC() {
         <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
         <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
         <li><a id="wd-k" href="#/Kanbas">Kanbas</a></li>
+        <li><a id="wd-github"
+               href="https://github.com/fyfly9090/kanbas-react-web-app.git">
+            Github            
+            </a>
+        </li>
       </ul>
     );
   }
