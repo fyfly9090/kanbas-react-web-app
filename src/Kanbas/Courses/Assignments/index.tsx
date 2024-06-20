4e33c1d8f2cb9f9f5f5835dddf29819bc4398115
fyfly9090 <fengyun0706@gmail.com>
2024-06-19 21:53:12 -0400

    updated database

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -3,15 +3,11 @@ import { BsGripVertical } from "react-icons/bs";
 import { IoMdArrowDropdown } from "react-icons/io";
 import AssignmentsControlButtons from "./AssignmentsControlButtons";
 import { GiNotebook } from "react-icons/gi";
-/* import Assignment01 from "./Assignment01"; */
 import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheck from "./GreenCheck";
-/* import Assignment02 from "./Assignment02";
-import Assignment03 from "./Assignment03"; */
 import { useParams } from "react-router";
 import * as db from "../../Database";
 import { Link } from "react-router-dom";
-
 
 export default function Assignments() {
     const { cid } = useParams();
@@ -30,7 +26,7 @@ export default function Assignments() {
             <ul className="wd-inassignments list-group rounded-0">
               {assignments
                  .filter((assignment: any) => assignment.course === cid)
-                 .map(({_id, title, available_date, due_date}) => (
+                 .map(({_id, title, available_date, due_date, points}) => (
                 <li className="wd-assignment list-group-item p-3 ps-1 border-left">
                  <div className="container-grid">                
                     <div className="col static-element"><BsGripVertical className="me-2 fs-3 centered-element" /></div>  
@@ -46,7 +42,7 @@ export default function Assignments() {
                             <span className="space-ele-right">{available_date}</span>|
                             <strong className="space-ele-left space-sm-right">Due</strong>
                             <span className="space-ele-right">{due_date}</span>| 
-                            <span className="space-ele-left">100pts</span>
+                            <span className="space-ele-left">{points}pts</span>
                           </span> 
                         </div> 
                       </span>
