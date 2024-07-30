a51a49de0490a5831538c872cad9cd2d053f0127
fyfly9090 <fengyun0706@gmail.com>
2024-07-29 21:04:19 -0400

    Updated styling

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -28,26 +28,40 @@ export default function Profile() {
     <div className="wd-profile-screen">
       <h1>Profile</h1>
       {profile && (
-        <div>
-          <input className="wd-username" value={profile.username}
+        <div className="aligh-left">
+          <div>
+            <input className="wd-username profile-style" value={profile.username}
                  onChange={(e) => setProfile({ ...profile, username:  e.target.value })}/>
-          <input className="wd-password" value={profile.password}
+          </div>
+          <div>
+            <input className="wd-password profile-style" value={profile.password}
                  onChange={(e) => setProfile({ ...profile, password:  e.target.value })}/>
-          <input className="wd-firstname" value={profile.firstName}
+          </div>
+          <div>
+            <input className="wd-firstname profile-style" value={profile.firstName}
                  onChange={(e) => setProfile({ ...profile, firstName: e.target.value })}/>
-          <input className="wd-lastname" value={profile.lastName}
+          </div>
+          <div>
+            <input className="wd-lastname profile-style" value={profile.lastName}
                  onChange={(e) => setProfile({ ...profile, lastName:  e.target.value })}/>
-          <input className="wd-dob" value={profile.dob}
+          </div>
+          <div>
+            <input className="wd-dob profile-style" value={profile.dob}
                  onChange={(e) => setProfile({ ...profile, dob: e.target.value })} type="date"/>
-          <input className="wd-email" value={profile.email}
+          </div>
+          <div>
+            <input className="wd-email profile-style" value={profile.email}
                  onChange={(e) => setProfile({ ...profile, email: e.target.value })}/>
-          <select className="wd-role" onChange={(e) => setProfile({ ...profile, role: e.target.value })}>
+          </div>
+          <select className="wd-role profile-style" onChange={(e) => setProfile({ ...profile, role: e.target.value })}>
             <option value="USER">User</option>            <option value="ADMIN">Admin</option>
             <option value="FACULTY">Faculty</option>      <option value="STUDENT">Student</option>
           </select>
-          <button onClick={signout} className="wd-signout-btn btn btn-danger w-100">
+          <div>
+          <button onClick={signout} className="wd-signout-btn btn btn-danger width-style">
             Sign out
           </button>
+          </div>
         </div>
       )}
     </div>
